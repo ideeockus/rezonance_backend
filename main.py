7e7e85b15886138de7220d2a8966e8008d8fa3c0
radmirkus <radmirka745@gmail.com>
2022-09-26 00:34:49 +0300

    Writed TODOs

--- main.py
+++ main.py
@@ -12,3 +12,10 @@ storage layer
 @app.get("/")
 async def abc():
     return {"abc": "123"}
+
+
+# TODO add README
+# TODO add LICENCE
+
+# TODO add models Event, Community, Channel
+# TODO add code for storage layer (postgresql, psycopg3)
